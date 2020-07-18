eb5b086730730b898c693f8256077f7f180374ee
Lukasz Sobel <lukasz.sobel@hotmail.com>
2020-07-18 19:29:34 +0200

    add FAQ components with FAQ.js file and working faq subpage

---
+++ src/components/FAQ/FAQ.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Container from '../Container/Container';
+
+const FAQ  = () => (
+  <Container>
+    <h2>FAQ</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    <h3>Question 1</h3>
+    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    <h3>Question 2</h3>
+    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h3>Question 3</h3>
+    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+  </Container>
+);
+  
+export default FAQ;
